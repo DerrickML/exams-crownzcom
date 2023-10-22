d06c50674474d42c00452444d1f6d542baa89cb6
CodeSandbox <support@codesandbox.io>
2023-10-22 10:37:09 +0000

    Sandbox snapshot commit

---
+++ appwriteServerConfig.js
@@ -0,0 +1,14 @@
+// Importing required modules and dotenv package
+import { Client, Users, Account } from "node-appwrite";
+
+// Init SDK
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1") // Setting your Appwrite endpoint from env var
+  .setProject("651413f38aee140189c2") // Setting your project ID from env var
+  .setKey(
+    "70d1462daa53310a65f8f7294464190bd60cbb8ca1a2b5f2b94763b086be5459463c75ae23eeef1e2804ecc8110de3cdc7d9457f15e2608700b8aa1a8db83bd44da2e93d5bbf4f8918dadc2d1f596b74c0e44ac07c9e7296228728e986060f29057a739c2ff62f426ff5cb8c26a1d2eb61af30ba048122b326378df46080aef2",
+  ); // Setting your secret API key from env var
+const account = new Account(client);
+const users = new Users(client);
+
+export { client, account, users };
--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-console.log("Hello CodeSandbox");
+// Importing required modules
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { client, users, account } from "./appwriteServerConfig.js";
+
+// Initializing Express app
+const app = express();
+
+/************************************************/
+/*From any origin*/
+// Use cors middleware with wildcard origin
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+/************************************************/
+
+// Server is set up to parse JSON bodies
+app.use(express.json());
+
+// Serving static files from 'public' directory
+app.use(express.static(path.join(process.cwd(), "public")));
+
+// ===== ROUTE HANDLERS =====
+
+/*ROUTE: User Listing*/
+app.get("/users", async (req, res) => {
+  try {
+    const usersList = await users.list(); // Assuming nodeAppwriteUsers is available
+    res.json({ users: usersList.users }); // Respond with the list of users
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch users" }); // Updated to return JSON
+  }
+});
+
+/*ROUTE: User Deletion*/
+app.post("/delete-user", async (req, res) => {
+  const userIds = req.body.userIds;
+  if (!userIds || userIds.length === 0) {
+    return res.status(400).send("User IDs are required");
+  }
+
+  const errors = []; // Array to hold any errors that occur
+
+  for (const userId of userIds) {
+    try {
+      await users.delete(userId); // Assuming nodeAppwriteUsers is available
+    } catch (error) {
+      console.error(error);
+      errors.push(`Failed to delete user with ID ${userId}: ${error.message}`);
+    }
+  }
+
+  if (errors.length > 0) {
+    res.status(500).json({ errors }); // Send back any errors that occurred
+  } else {
+    res.send("Users deleted successfully");
+  }
+});
+
+// ===== STARTING THE SERVER =====
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
