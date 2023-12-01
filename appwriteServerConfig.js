205ced537646fcc22748bc2c3ef4f2cb8888e561
CodeSandbox <support@codesandbox.io>
2023-12-01 09:18:46 +0000

    Sandbox snapshot commit

--- appwriteServerConfig.js
+++ appwriteServerConfig.js
@@ -13,9 +13,7 @@ import {
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1") // Setting your Appwrite endpoint from env var
   .setProject("651413f38aee140189c2") // Setting your project ID from env var
-  .setKey(
-    "70d1462daa53310a65f8f7294464190bd60cbb8ca1a2b5f2b94763b086be5459463c75ae23eeef1e2804ecc8110de3cdc7d9457f15e2608700b8aa1a8db83bd44da2e93d5bbf4f8918dadc2d1f596b74c0e44ac07c9e7296228728e986060f29057a739c2ff62f426ff5cb8c26a1d2eb61af30ba048122b326378df46080aef2",
-  ); // Setting your secret API key from env var
+  .setKey(process.env.APPWRITE_API_KEY); // Setting your secret API key from env var
 const account = new Account(client);
 const users = new Users(client);
 const databases = new Databases(client);
