17fe430721f7520312eb51f1c2695d12e2629f14
DeroMal <derrickmal123@gmail.com>
2024-04-11 13:19:39 +0300

    - bug fix

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serving static files from 'public' directory
-// app.use(express.static(path.join(process.cwd(), "public")));
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(path.join(process.cwd(), "public")));
+// app.use(express.static(path.join(process.cwd(), 'public')));
 
 
 // Generate random password
@@ -493,9 +493,9 @@ app.use("/query", queryCachedDbRoutes);
 
 // The "catch-all" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(process.cwd(), 'public', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(process.cwd(), 'public', 'index.html'));
+// });
 
 // ===== STARTING THE SERVER =====
 app.listen(PORT_NO, () => {
